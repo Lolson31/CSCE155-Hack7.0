2bbef8552130dc48a2751ddc46feca5ec3878bc1
Lolson31 <69780105+Lolson31@users.noreply.github.com>
2020-10-05 18:33:11 -0500

    turned newArray in paddedCopy into a dynamic array

--- array_utils.c
+++ array_utils.c
@@ -65,7 +65,7 @@ int containsWithin (const int *arr, int size, int x, int i, int j)
 
 int * paddedCopy (const int *arr, int oldSize, int newSize)
 {
-  int newArray[newSize];
+  int *newArray = (int*) malloc(newSize * sizeof(int));
 
   for (int i = 0; i < newSize; i++)
   {
