2c9d902a3ba93ddea37e31a060e4141d44e9750a
Lolson31 <69780105+Lolson31@users.noreply.github.com>
2020-10-05 12:39:14 -0500

    created functions contains, containsWithin, paddedCopy, reverse, and reverseCopy

--- array_utils.h
+++ array_utils.h
@@ -4,3 +4,13 @@
  *
  * This program will
  */
+
+int contains (const int *arr, int size, int x);  //Determines if an array contains int x
+
+int containsWithin (const int *arr, int size, int x, int i, int j); //Determines if array contains x within the range i - j
+
+int * paddedCopy (const int *arr, int oldSize, int NewSize); //Creates a copy of an array with the size NewSize
+
+void  reverse (int *arr, int size); //Reverses array order
+
+int * reverseCopy (const int *arr, int size); //Makes copy of array with elements in reverse order
