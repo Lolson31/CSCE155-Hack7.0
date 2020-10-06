434b31ce061e3bd45ba245039403b01ad97e9f89
Lolson31 <69780105+Lolson31@users.noreply.github.com>
2020-10-05 19:03:29 -0500

    Update array_utils.h

--- array_utils.h
+++ array_utils.h
@@ -11,10 +11,10 @@ int containsWithin (const int *arr, int size, int x, int i, int j); //Determines
 
 int * paddedCopy (const int *arr, int oldSize, int newSize); //Creates a copy of an array with the size NewSize
 
-void  reverse (int *arr, int size); //Reverses array order
+int  reverse (int *arr, int size); //Reverses array order
 
 int * reverseCopy (const int *arr, int size); //Makes copy of array with elements in reverse order
 
 void testCheckFunc (int func);  //Tests if a function that checks an array returns 1 (true) or 0 (false)
 
-void testPaddedCopy (int *func, int newArraySize); //Returns the result of a function that edits an array
+void testEditFunc (int *func, int newArraySize); //Returns the result of a function that edits an array
